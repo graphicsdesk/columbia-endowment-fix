9c6857714d2234e4c2f707c78efffc3047d68714
laya-gollamudi <layagollamudi@gmail.com>
2022-08-30 15:50:44 -0400

    hi

--- src/script.js
+++ src/script.js
@@ -1,29 +1,22 @@
+import enterView from 'enter-view';
 import textBalancer from 'text-balancer';
-import initiatePage from './scripts/page';
-import { intersectTop } from './scripts/utils';
-
-import { spectate as spectateConfig } from '../package.json';
-
-// Main page initiation
-
-initiatePage();
+import { USE_COVER_HED, USE_EYE_NAV } from '../config.json';
+import './scripts/page';
 
 // Fade in navbar at scroll trigger
 
 const navbar = document.getElementById('navbar');
 
-const { USE_NEWS_NAV, USE_EYE_NAV, USE_COVER_HED } = spectateConfig;
-if (USE_NEWS_NAV || USE_EYE_NAV || USE_COVER_HED) {
-  intersectTop({
-    node: document.getElementById('headline'),
-    onEnter: () => {
-      navbar.classList.remove('only-eye-logo');
-      navbar.classList.remove('hide-news-navbar');
+if (USE_COVER_HED || USE_EYE_NAV) {
+  enterView({
+    selector: USE_COVER_HED ? '.headline' : '.step-deck',
+    offset: USE_COVER_HED ? 1 : 0.957,
+    enter: () => {
+      navbar.classList.remove('only-logo');
     },
-    onExit: () => {
+    exit: () => {
       navbar.classList.remove('show-nav-links');
-      navbar.classList.add('only-eye-logo');
-      navbar.classList.add('hide-news-navbar');
+      navbar.classList.add('only-logo');
     },
   });
 }
@@ -36,6 +29,5 @@ export function hamburgerTrigger() {
 
 // Text balance headline, deck, and image captions
 
-if (window.innerWidth <= 460) {
-  textBalancer.balanceText('#headline, .deck, .image-caption-text');
-}
+if (window.innerWidth <= 460)
+  textBalancer.balanceText('.headline, .deck, .image-overlay .image-caption-text');
